7079e591cc062211fbce6ff99c5050447433221a
Daniel Callif <Daniel Callif>
2017-05-17 16:52:40 -0600

    First Commit

---
+++ WebContent/login.jsp
@@ -0,0 +1,22 @@
+<%@page import="com.dcallif.util.DriverManagerClass"%>
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.sql.Statement"%>
+<%@page import="java.sql.Connection"%>
+<%
+	String userName = request.getParameter("username");
+	String password = request.getParameter("password");
+	
+	DriverManagerClass driverManager = new DriverManagerClass();
+	
+	Connection con = driverManager.createConnection( "jdbc:mysql://127.0.0.1:3306/employees", "root", "admin" );
+	Statement st = con.createStatement();
+	ResultSet rs;
+	rs = st.executeQuery(
+			"select * from EMPLOYEE where USER_NAME='" + userName + "' and PASSWORD='" + password + "'");
+	if (rs.next()) {
+		session.setAttribute("username", userName);
+		response.sendRedirect("success.jsp");
+	} else {
+		out.println("Invalid password <a href='index.jsp'>try again</a>");
+	}
+%>
---
+++ src/com/dcallif/util/DriverManagerClass.java
@@ -0,0 +1,54 @@
+package com.dcallif.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DriverManagerClass 
+{
+	String driverName = "com.mysql.jdbc.Driver";
+	Connection con = null;
+	
+	public DriverManagerClass() 
+	{
+		try 
+		{
+			//Loading Driver for MySql
+			Class.forName(driverName);
+		} 
+		catch (ClassNotFoundException e) 
+		{
+			System.out.println(e.toString());
+		}
+	}
+	
+	//Tries to make a new connection to specified database
+	public Connection createConnection(String connectionUrl, String userName, String userPass) 
+	{
+		try 
+		{
+			con = DriverManager
+					.getConnection(connectionUrl,userName,userPass);
+			System.out.println("******* Connection created successfully........");
+		} 
+		catch (SQLException e) 
+		{
+			System.out.println(e.toString());
+		}
+		return con;
+	}
+	
+	//Tries to close a database connection
+	public void closeConnection()
+	{
+		try
+		{
+			this.con.close();
+			System.out.println("******* Connection closed Successfully.........");
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.toString());
+		}
+	}
+}
